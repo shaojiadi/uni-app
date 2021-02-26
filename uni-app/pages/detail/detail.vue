7621cf7765da7d1e563f4172b085360f86c38817
邵佳迪 <1923943838qq.com>
2021-02-26 17:20:53 +0800

    uni image

---
+++ uni-app/pages/detail/detail.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<view class="box2" hover-class="box2-active">
+			<view class="box"  :hover-start-time="2000"   hover-class="box-active" hover-stop-propagation>详情页</view>
+		</view>
+		<view>
+			<text selectable="true">武动乾坤</text>
+		</view>
+		<view>
+			<text space="nbsp">创建       下周</text>
+		</view>
+		<view>
+			<text space="emsp">创建       下周</text>
+		</view>
+		<image src="http://destiny001.gitee.io/image/cxk.gif" mode="aspectFill"></image>
+		<button size="mini" type="primary" plain>按钮</button>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.box {
+		width: 100px;
+		height: 100px;
+		background-color: #007AFF;
+	}
+	.box2 {
+		width: 200px;
+		height: 200px;
+		background-color: #DD524D;
+	}
+	
+	.box2-active {
+		background-color: #4CD964;
+	}
+	.box-active {
+		background-color: pink;
+	}
+	
+</style>
---
+++ uni-app/pages/uni-style/uni-style.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<view>
+			样式study
+		</view>
+		<view class="box1">
+			文字
+			<text>123</text>
+		</view>
+		<view class="iconfont .icon-01hangzhengguanli"></view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+	@import url("./a.css");
+	.box1 {
+		width: 750rpx;
+		height: 750rpx;
+		background-color: #4CD964;
+		font-size: 24rpx;
+		color: #fff;
+		text {
+			color: pink;
+		}
+	}
+</style>
